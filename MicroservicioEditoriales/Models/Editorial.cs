19b03a0f21d93e66a0323401ea29f124ddf6134b
Samuel <smaedam@miumg.edu.gt>
2025-08-29 11:39:04 -0600

    Primer Git

---
+++ MicroservicioEditoriales/Models/Editorial.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroservicioEditoriales.Models
+{
+    public class Editorial
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Pais { get; set; } = string.Empty;
+    }
+}
